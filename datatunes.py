6982d9c289cedf199eefc2942b313c533ccf4309
jessffff <122911304+jessffff@users.noreply.github.com>
2023-05-12 08:27:55 +0200

    Update datatunes.py

--- datatunes.py
+++ datatunes.py
@@ -1,9 +1,12 @@
+#
 import pandas as pd
 import streamlit as st
 import requests
 import toml
-from PIL import Image
-
+from PIL import Image  
+from genres import genre_bornes
+from genres import genre_radio
+from genres import genres
 
 hide_st_style = """
             <style>
@@ -14,183 +17,12 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-
 image=Image.open('STARTER.png')
 st.image(image,use_column_width=True)
 
 
-genres=('disco','electro','hiphop','house','pop','jazz','metal','r&b','ragga','rock','classical','country','latin','bossa nova')
 diffusion=('Radio','Online','Vinyles')
-genre_bornes = {
-    'disco': {
-        'danceability': (6.0, 10.0),
-        'energy': (6.0, 10.0),
-        'loudness': (-30.0, -10.0),
-        'speechiness': (2.0, 8.0),
-        'acousticness': (0.0, 4.0),
-        'instrumentalness': (0.0, 6.0),
-        'liveness': (4.0, 10.0),
-        'valence': (6.0, 10.0),
-        'tempo': (100.0, 140.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'electro': {
-        'danceability': (7.0, 10.0),
-        'energy': (7.0, 10.0),
-        'loudness': (-30.0, -5.0),
-        'speechiness': (2.0, 6.0),
-        'acousticness': (0.0, 6.0),
-        'instrumentalness': (6.0, 10.0),
-        'liveness': (4.0, 10.0),
-        'valence': (4.0, 8.0),
-        'tempo': (120.0, 160.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'hiphop': {
-        'danceability': (5.0, 8.0),
-        'energy': (5.0, 8.0),
-        'loudness': (-30.0, -5.0),
-        'speechiness': (6.0, 10.0),
-        'acousticness': (0.0, 6.0),
-        'instrumentalness': (0.0, 6.0),
-        'liveness': (4.0, 10.0),
-        'valence': (2.0, 8.0),
-        'tempo': (60.0, 100.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'house': {
-        'danceability': (7.0, 10.0),
-        'energy': (7.0, 10.0),
-        'loudness': (-30.0, -5.0),
-        'speechiness': (2.0, 6.0),
-        'acousticness': (0.0, 6.0),
-        'instrumentalness': (6.0, 10.0),
-        'liveness': (4.0, 10.0),
-        'valence': (4.0, 8.0),
-        'tempo': (120.0, 140.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'pop': {
-        'danceability': (6.0, 9.0),
-        'energy': (4.0, 8.0),
-        'loudness': (-30.0, -5.0),
-        'speechiness': (2.0, 8.0),
-        'acousticness': (0.0, 6.0),
-        'instrumentalness': (6.0, 10.0),
-        'liveness': (4.0, 10.0),
-        'valence': (4.0, 8.0),
-        'tempo': (120.0, 140.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'jazz': {
-        'danceability': (7.0, 10.0),
-        'energy': (7.0, 10.0),
-        'loudness': (-30.0, -5.0),
-        'speechiness': (2.0, 6.0),
-        'acousticness': (0.0, 6.0),
-        'instrumentalness': (6.0, 10.0),
-        'liveness': (4.0, 10.0),
-        'valence': (4.0, 8.0),
-        'tempo': (120.0, 160.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'metal': {
-        'danceability': (6.0, 10.0),
-        'energy': (6.0, 10.0),
-        'loudness': (-30.0, -10.0),
-        'speechiness': (2.0, 8.0),
-        'acousticness': (0.0, 4.0),
-        'instrumentalness': (0.0, 6.0),
-        'liveness': (4.0, 10.0),
-        'valence': (6.0, 10.0),
-        'tempo': (100.0, 140.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'r&b': {
-        'danceability': (7.0, 10.0),
-        'energy': (7.0, 10.0),
-        'loudness': (-30.0, -5.0),
-        'speechiness': (2.0, 6.0),
-        'acousticness': (0.0, 6.0),
-        'instrumentalness': (6.0, 10.0),
-        'liveness': (4.0, 10.0),
-        'valence': (4.0, 8.0),
-        'tempo': (120.0, 140.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'ragga': {
-        'danceability': (6.0, 9.0),
-        'energy': (4.0, 8.0),
-        'loudness': (-30.0, -5.0),
-        'speechiness': (2.0, 8.0),
-        'acousticness': (0.0, 6.0),
-        'instrumentalness': (6.0, 10.0),
-        'liveness': (4.0, 10.0),
-        'valence': (4.0, 8.0),
-        'tempo': (120.0, 140.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'rock': {
-        'danceability': (6.0, 10.0),
-        'energy': (6.0, 10.0),
-        'loudness': (-30.0, -10.0),
-        'speechiness': (2.0, 8.0),
-        'acousticness': (0.0, 4.0),
-        'instrumentalness': (0.0, 6.0),
-        'liveness': (4.0, 10.0),
-        'valence': (6.0, 10.0),
-        'tempo': (100.0, 140.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'classical': {
-        'danceability': (6.0, 10.0),
-        'energy': (6.0, 10.0),
-        'loudness': (-30.0, -10.0),
-        'speechiness': (2.0, 8.0),
-        'acousticness': (0.0, 4.0),
-        'instrumentalness': (0.0, 6.0),
-        'liveness': (4.0, 10.0),
-        'valence': (6.0, 10.0),
-        'tempo': (100.0, 140.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'country': {
-        'danceability': (5.0, 8.0),
-        'energy': (5.0, 8.0),
-        'loudness': (-30.0, -5.0),
-        'speechiness': (6.0, 10.0),
-        'acousticness': (0.0, 6.0),
-        'instrumentalness': (0.0, 6.0),
-        'liveness': (4.0, 10.0),
-        'valence': (2.0, 8.0),
-        'tempo': (60.0, 100.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'latin': {
-        'danceability': (6.0, 10.0),
-        'energy': (6.0, 10.0),
-        'loudness': (-30.0, -10.0),
-        'speechiness': (2.0, 8.0),
-        'acousticness': (0.0, 4.0),
-        'instrumentalness': (0.0, 6.0),
-        'liveness': (4.0, 10.0),
-        'valence': (6.0, 10.0),
-        'tempo': (100.0, 140.0),
-        'duration_ms': (180000.0, 300000.0)
-    },
-    'bossa nova': {
-        'danceability': (6.0, 10.0),
-        'energy': (6.0, 10.0),
-        'loudness': (-30.0, -10.0),
-        'speechiness': (2.0, 8.0),
-        'acousticness': (0.0, 4.0),
-        'instrumentalness': (0.0, 6.0),
-        'liveness': (4.0, 10.0),
-        'valence': (6.0, 10.0),
-        'tempo': (100.0, 140.0),
-        'duration_ms': (180000.0, 300000.0)
-    } 
-}
+
 Avg_Pay_per_Stream = {
     0.00154: 'YouTube',
     0.00203: 'Pandora',
@@ -207,30 +39,8 @@ data = {'Distributeur': ['Distro kid', 'Tunecore', 'AWAL', 'Ditto', 'CD Baby'],
         'Price': ['20$/year', '30$/year', '15% fee', '19$/year', '5$/album + 9% fee']}
 distrib_spotify = pd.DataFrame(data)
 
-genre_radio = {
-    'France Inter': 'années 80, pop musique, rap, jazz, chanson française, rock',
-    'RMC': 'pop, rock, rap, electro, world, jazz, chanson française',
-    'Franceinfo': 'pop, rock, rap, electro, world, jazz, chanson française',
-    'RTL': 'pop rock, pop',
-    'NRJ': 'chanson française, dance, dancehall, electro, funk, gangsta rap, hiphop, latino, pop, pop-rock, rap, reggae, reggaeton, r&b, rock, soul, techno, zouk, electro, house, latin',
-    'Nostalgie': 'disco, années 80, années 90, country',
-    'FIP': 'jazz, electro, rock, pop, groove',
-    'Cherie FM': 'féminin, pop rock, pop',
-    'Skyrock': 'rap, r&b, ragga',
-    'France Culture': 'classique, chanson française',
-    'RTL2': 'pop rock, rock',
-    'Radio Classique': 'classical, bossa nova',
-    'Fun Radio': 'electro, dance, Techno, house, latin',
-    'Rire et Chansons': 'pop rock, chanson française, rock',
-    'Sud Radio': 'pop rock, pop, chanson française',
-    'Radio Orient': 'chanson arabe',
-    'France Musique': 'classical, jazz, bossa nova',
-    'M Radio': 'chanson française',
-    'Chante France': 'chanson française',
-    'Oui FM': 'rock, punk, electro, house, metal, country'
-}
-df_genre_radio = pd.DataFrame.from_dict(genre_radio, orient='index', columns=['genres'])
 
+df_genre_radio = pd.DataFrame.from_dict(genre_radio, orient='index', columns=['genres'])
 
 
 
@@ -241,6 +51,7 @@ def creation_onglet():
         with col1:
             genre_input = st.selectbox("", genres, format_func=lambda x: x)
             genre_bornes_selected = genre_bornes[genre_input]
+
     col1, col2 = st.columns([1,1])
     with st.form("form 2"):
         with col1:
@@ -267,7 +78,6 @@ def creation_onglet():
             duration_ms_min, duration_ms_max = genre_bornes_selected['duration_ms']
             duration_ms = st.slider('Durée : ', duration_ms_min, duration_ms_max)
         submit1 = st.form_submit_button("Submit")
- 
 
 
 def diffusion_onglet():
@@ -332,14 +142,10 @@ def diffusion_onglet():
 
 
 
-
-
-
 tabs = {
     "Création": creation_onglet,
     "Diffusion": diffusion_onglet
 }
 selection = st.sidebar.radio("Sélectionnez un onglet", list(tabs.keys()))
 tabs[selection]()
-
 
