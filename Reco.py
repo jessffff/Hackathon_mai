009d487dffb1b8a816303e17452cd3377d966cf9
jessffff <122911304+jessffff@users.noreply.github.com>
2023-05-12 13:16:02 +0200

    Update Reco.py

--- Reco.py
+++ Reco.py
@@ -3,10 +3,8 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-df = pd.read_csv('Dataset_clean_2.csv')
+df = pd.read_csv('Dataset_clean_3.csv')
 
-
-df = df.drop(columns = ['Unnamed: 0','id_artists','id_artists','id_tracks_df','popularity_artists_df','followers','time_signature',"duration_ms"])
 
 
 def reco(df,genre,features):
@@ -21,6 +19,6 @@ def reco(df,genre,features):
 
     recommended_tracks = filtered_df.sort_values('similarity', ascending=False)[:5]
     
-    recommended_tracks = recommended_tracks[['name_artists_df','name_tracks_df','release_year','duration_m','popularity_tracks_df','danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']]
+    recommended_tracks = recommended_tracks[['name_artists','name_tracks','release_year','duration_m','popularity_tracks','danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']]
 
     return recommended_tracks
